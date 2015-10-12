147f12007a9c0132200e1b810e30e73d733209a9
Jon Eyolfson <jon@eyl.io>
2015-10-12 20:32:58 +0200

    Added more wayland classes

--- src/gui/main.cxx
+++ src/gui/main.cxx
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include <stdio.h>
 #include <cstring>
 
 #include <linux/memfd.h>
@@ -28,24 +28,28 @@
 #include <wayland-client-protocol.h>
 #include "xdg_shell.h"
 
-namespace wayland
-{
+namespace wayland {
 
-class display
-{
+class display {
 	struct wl_display *ptr;
 
 	friend class registry;
 
 public:
-	display() { ptr = wl_display_connect(nullptr); }
+	display()
+	{
+		ptr = wl_display_connect(nullptr);
+	}
 	~display()
 	{
 		if (ptr != nullptr) {
 			wl_display_disconnect(ptr);
 		}
 	}
-	bool has_errors() { return ptr == nullptr; }
+	bool has_errors()
+	{
+		return ptr == nullptr;
+	}
 	void dispatch() const
 	{
 		if (ptr != nullptr) {
@@ -60,8 +64,7 @@ public:
 	}
 };
 
-class compositor
-{
+class compositor {
 	struct wl_compositor *ptr;
 
 	compositor() : ptr(nullptr) {}
@@ -71,7 +74,10 @@ class compositor
 		ptr = static_cast<struct wl_compositor *>(p);
 	}
 
-	bool has_errors() { return ptr == nullptr; }
+	bool has_errors()
+	{
+		return ptr == nullptr;
+	}
 
 	~compositor()
 	{
@@ -81,15 +87,66 @@ class compositor
 	}
 
 	friend class registry;
+	friend class surface;
 };
 
-class shell
-{
+namespace xdg { class surface; }
+
+class surface {
+	struct wl_surface *ptr;
+
+public:
+	surface(const compositor &c)
+	{
+		ptr = wl_compositor_create_surface(c.ptr);
+
+	}
+	~surface()
+	{
+		if (ptr != nullptr) {
+			wl_surface_destroy(ptr);
+		}
+	}
+
+	friend class xdg::surface;
+};
+
+class shm {
+	struct wl_shm *ptr;
+
+	shm() : ptr(nullptr) {}
+
+	void initialize(void *p)
+	{
+		ptr = static_cast<struct wl_shm *>(p);
+	}
+
+	bool has_errors()
+	{
+		return ptr == nullptr;
+	}
+
+	~shm()
+	{
+		if (ptr != nullptr) {
+			wl_shm_destroy(ptr);
+		}
+	}
+
+	friend class registry;
+};
+
+class registry;
+
+namespace xdg {
+
+class shell {
 	struct xdg_shell *ptr;
 
 	static struct xdg_shell_listener listener;
 
-	static void ping(void *data, struct xdg_shell *xdg_shell,
+	static void ping(void *data,
+			 struct xdg_shell *xdg_shell,
 			 uint32_t serial)
 	{
 		xdg_shell_pong(xdg_shell, serial);
@@ -103,11 +160,14 @@ class shell
 		if (ptr != nullptr) {
 			xdg_shell_add_listener(ptr, &listener, nullptr);
 			xdg_shell_use_unstable_version(
-			    ptr, XDG_SHELL_VERSION_CURRENT);
+				ptr, XDG_SHELL_VERSION_CURRENT);
 		}
 	}
 
-	bool has_errors() { return ptr == nullptr; }
+	bool has_errors()
+	{
+		return ptr == nullptr;
+	}
 
 	~shell()
 	{
@@ -116,34 +176,56 @@ class shell
 		}
 	}
 
-	friend class registry;
+	friend class wayland::registry;
+	friend class surface;
 };
 
-class shm
-{
-	struct wl_shm *ptr;
+class surface {
+	struct xdg_surface *ptr;
 
-	shm() : ptr(nullptr) {}
+	static struct xdg_surface_listener listener;
 
-	void initialize(void *p) { ptr = static_cast<struct wl_shm *>(p); }
-
-	bool has_errors() { return ptr == nullptr; }
-
-	~shm()
+	static void configure(void *data, struct xdg_surface *xdg_surface,
+			      int32_t width, int32_t height,
+			      struct wl_array *states, uint32_t serial)
 	{
-		if (ptr != nullptr) {
-			wl_shm_destroy(ptr);
-		}
+		xdg_surface_ack_configure(xdg_surface, serial);
+	}
+	static void close(void *data, struct xdg_surface *xdg_surface)
+	{
 	}
 
-	friend class registry;
+public:
+	surface(shell &sh, wayland::surface &su)
+	{
+		ptr = xdg_shell_get_xdg_surface(sh.ptr, su.ptr);
+	}
+	void set_title(const char *title)
+	{
+		if (ptr != nullptr) {
+			xdg_surface_set_title(ptr, title);
+		}
+	}
+	void set_window_geometry(int32_t x, int32_t y, int32_t w, int32_t h)
+	{
+		if (ptr != nullptr) {
+			xdg_surface_set_window_geometry(ptr, x, y, w, h);
+		}
+	}
+	~surface()
+	{
+		if (ptr != nullptr) {
+			xdg_surface_destroy(ptr);
+		}
+	}
 };
 
-class registry
-{
+} // namespace xdg
+
+class registry {
 	struct wl_registry *ptr;
 	wayland::compositor compositor;
-	wayland::shell shell;
+	wayland::xdg::shell shell;
 	wayland::shm shm;
 
 	static struct wl_registry_listener listener;
@@ -176,16 +258,16 @@ public:
 		ptr = wl_display_get_registry(d.ptr);
 		if (ptr != nullptr) {
 			wl_registry_add_listener(ptr, &listener, this);
+			d.dispatch();
 		}
-		d.dispatch();
 	}
 	bool has_errors()
 	{
-		return ptr == nullptr || compositor.has_errors() ||
-		       shell.has_errors() || shm.has_errors();
+		return ptr == nullptr || compositor.has_errors()
+		       || shell.has_errors() || shm.has_errors();
 	}
 	wayland::compositor &compositor_ref() { return compositor; }
-	wayland::shell &shell_ref() { return shell; }
+	wayland::xdg::shell &shell_ref() { return shell; }
 	wayland::shm &shm_ref() { return shm; }
 	~registry()
 	{
@@ -194,21 +276,32 @@ public:
 		}
 	}
 };
-}
 
-struct xdg_shell_listener wayland::shell::listener = {ping};
+} // namespace wayland
 
-struct wl_registry_listener wayland::registry::listener = {global,
-							   global_remove};
+struct xdg_shell_listener wayland::xdg::shell::listener = {
+	ping
+};
 
-class pixels
-{
+struct xdg_surface_listener wayland::xdg::surface::listener = {
+	configure,
+	close
+};
+
+struct wl_registry_listener wayland::registry::listener = {
+	global,
+	global_remove
+};
+
+namespace {
+
+class pixels {
 	uint32_t *data;
-	const int32_t width;
-	const int32_t height;
+	const int32_t w;
+	const int32_t h;
 
 public:
-	pixels(int32_t w, int32_t h) : data(nullptr), width(w), height(h)
+	pixels(int32_t w, int32_t h) : data(nullptr), w(w), h(h)
 	{
 		if (w <= 0 || h <= 0) {
 			return;
@@ -227,13 +320,27 @@ public:
 		}
 		close(fd);
 	}
+	int32_t width() const
+	{
+		return w;
+	}
+	int32_t height() const
+	{
+		return h;
+	}
 	int32_t capacity() const
 	{
-		return width * height * sizeof(uint32_t) * 2;
+		return w * h * sizeof(uint32_t);
 	}
-	int32_t stride() const { return width * sizeof(uint32_t); }
+	int32_t stride() const
+	{
+		return w * sizeof(uint32_t);
+	}
 	~pixels() { munmap(data, capacity()); }
 };
+
+} // namespace
+
 int main(int argc, char **argv)
 {
 	wayland::display display;
@@ -246,6 +353,12 @@ int main(int argc, char **argv)
 	}
 	wayland::compositor &compositor = registry.compositor_ref();
 	wayland::shm &shm = registry.shm_ref();
-	wayland::shell &shell = registry.shell_ref();
+	wayland::xdg::shell &shell = registry.shell_ref();
+
+	wayland::surface surface(compositor);
+	wayland::xdg::surface xdg_surface(shell, surface);
+
 	pixels ps(300, 200);
+	xdg_surface.set_title("Eyl Lang");
+	xdg_surface.set_window_geometry(10, 10, ps.width(), ps.height());
 }
